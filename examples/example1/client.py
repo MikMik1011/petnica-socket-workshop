c2e67635b083f77e81dd93e7a1aa95260a61ec90
Misa Stefanovic <mikmikyt1011@gmail.com>
2023-08-09 22:35:00 +0200

    add basic hello world example

---
+++ examples/example1/client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <ip> <port>")
+    sys.exit(1)
+
+SERVER_IP = sys.argv[1]
+SERVER_PORT = int(sys.argv[2])
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((SERVER_IP, SERVER_PORT))
+sock.send(b"Hello, world!")
+
+data = sock.recv(1024)
+print(f"[SERVER] {data.decode()}")
+
+sock.close()
