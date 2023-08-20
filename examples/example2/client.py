05125febefa77c7b7891e33eb8440ae793f74c84
Misa Stefanovic <mikmikyt1011@gmail.com>
2023-08-20 04:48:25 +0200

    prettier header handling in second example

--- examples/example2/client.py
+++ examples/example2/client.py
@@ -5,39 +5,37 @@ if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} <ip> <port>")
     sys.exit(1)
 
-def encodeHeader(message, name):
-    header = str(len(message)).encode()
-    header += b'\x00\x69\x00'
-    header += name[:64 - len(header)].encode()
-    header += b"\x00" * (64 - len(header))
-
-    return header
-
-def decodeHeader(header):
-    data = header.split(b'\x00\x69\x00')
-    msgSize = int(data[0].decode())
-    name = data[1].decode().rstrip('\x00')
-    return msgSize, name
-
 SERVER_IP = sys.argv[1]
 SERVER_PORT = int(sys.argv[2])
+HEADER_SIZE_BODY_LEN = 10
+HEADER_SIZE_NAME_LEN = 30
+
+def appendHeaders(message, name):
+    return bytes(f"{len(message):<{HEADER_SIZE_BODY_LEN}}" + f"{name:<{HEADER_SIZE_NAME_LEN}}" + message, "utf-8")
+
+def receiveMessage(conn, length):
+    msg = conn.recv(length)
+    if not msg:
+        raise Exception("Connection closed")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-name = input("Enter your name: ")
+username = input("Enter your name: ")
 sock.connect((SERVER_IP, SERVER_PORT))
 try:
     while True:
         msg = input("Enter your message: ")
-        header = encodeHeader(msg, name)
-        sock.send(header)
-        sock.send(msg.encode())
+        msg = appendHeaders(msg, username)
+        sock.send(msg)
 
-        header = sock.recv(64)
-        if not header:
+        try:
+            msgSize = int(sock.recv(HEADER_SIZE_BODY_LEN).decode())
+            name = sock.recv(HEADER_SIZE_NAME_LEN).decode().rstrip('\x00').strip()
+            msg = sock.recv(msgSize).decode()
+        except Exception as e:
+            print(f"Error: {e}")
             break
-        msgSize, name = decodeHeader(header)
-        msg = sock.recv(msgSize)
-        print(f"[{name}] {msg}")
+
+        print(f"[{name}] {msg}!")
 
 except KeyboardInterrupt:
     sock.close()
--- examples/example2/server.py
+++ examples/example2/server.py
@@ -6,42 +6,37 @@ if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <port>")
     sys.exit(1)
 
-def encodeHeader(message, name):
-    header = str(len(message)).encode()
-    header += b'\x00\x69\x00'
-    header += name[:64 - len(header)].encode()
-    header += b"\x00" * (64 - len(header))
-
-    return header
-
-def decodeHeader(header):
-    data = header.split(b'\x00\x69\x00')
-    msgSize = int(data[0].decode())
-    name = data[1].decode().rstrip('\x00')
-    return msgSize, name
-
 SERVER_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = int(sys.argv[1])
+HEADER_SIZE_BODY_LEN = 10
+HEADER_SIZE_NAME_LEN = 30
+
+def appendHeaders(message, name):
+    return bytes(f"{len(message):<{HEADER_SIZE_BODY_LEN}}" + f"{name:<{HEADER_SIZE_NAME_LEN}}" + message, "utf-8")
+
+def receiveMessage(conn, length):
+    msg = conn.recv(length)
+    if not msg:
+        raise Exception("Connection closed")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER_IP, SERVER_PORT))
 
-
 def handleConnection(conn, addr):
     while True:
-        header = conn.recv(64)
-        if not header:
+        try:
+            msgSize = int(conn.recv(HEADER_SIZE_BODY_LEN).decode())
+            name = conn.recv(HEADER_SIZE_NAME_LEN).decode().rstrip('\x00').strip()
+            msg = conn.recv(msgSize).decode()
+        except Exception as e:
+            print(f"Error: {e}")
             break
-
-        msgSize, name = decodeHeader(header)
-        msg = conn.recv(msgSize).decode()
 
         print(f"[{name}] {msg}!")
 
         response = f"{name} said: {msg}"
-        responseHeader = encodeHeader(response, "SERVER")
-        conn.send(responseHeader)
-        conn.send(response.encode())
+        response = appendHeaders(response, "SERVER")
+        conn.send(response)
 
     conn.close()
     print(f"Closed connection from {addr}")
