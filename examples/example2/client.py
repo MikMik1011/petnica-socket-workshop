fd0d7746ab3b7e034e9bbf863481b97fd1dd729e
Misa Stefanovic <mikmikyt1011@gmail.com>
2023-08-20 04:57:59 +0200

    fix buffer overflow in example2

--- examples/example2/client.py
+++ examples/example2/client.py
@@ -11,7 +11,7 @@ HEADER_SIZE_BODY_LEN = 10
 HEADER_SIZE_NAME_LEN = 30
 
 def appendHeaders(message, name):
-    return bytes(f"{len(message):<{HEADER_SIZE_BODY_LEN}}" + f"{name:<{HEADER_SIZE_NAME_LEN}}" + message, "utf-8")
+    return bytes(f"{len(message):<{HEADER_SIZE_BODY_LEN}}" + f"{name[:HEADER_SIZE_NAME_LEN].ljust(HEADER_SIZE_NAME_LEN)}" + message, "utf-8")
 
 def receiveMessage(conn, length):
     msg = conn.recv(length)
